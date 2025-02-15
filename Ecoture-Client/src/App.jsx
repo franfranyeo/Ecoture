2c3a68c5c2998ecec485328807d58a2e2e3bd0b1
jia-xinnnn <233506d@mymail.nyp.edu.sg>
2025-02-16 02:38:11 +0800

    Improved ui for livechat

--- Ecoture-Client/src/App.jsx
+++ Ecoture-Client/src/App.jsx
@@ -1,72 +1,73 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { useMemo, useRef } from 'react';
 import {
+  Navigate,
+  Route,
   BrowserRouter as Router,
   Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import MyTheme from "./themes/MyTheme";
-import Navbar from "components/Navbar";
-import http from "utils/http";
-import UserContext from "./contexts/UserContext";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useMemo, useRef } from "react";
+} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import http from 'utils/http';
+
+import { Box } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import ChatWidget from './components/ChatWidget';
+import ProtectedRoute from './components/admin/ProtectedRoute';
+import Navbar from 'components/Navbar';
+
+import UserContext from './contexts/UserContext';
 
 // KEON IMPORTS
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
-import ProductDetail from "./pages/ProductDetail";
-import Reviews from "./pages/Reviews";
-import Products from "./pages/Products";
-import CustomerLanding from "./pages/CustomerLanding";
-import StaffDashboard from "./pages/admin/products/AdminProducts";
-
+import AddProduct from './pages/AddProduct';
+import AddAddress from './pages/Checkout/AddAddress';
+import AddCreditCard from './pages/Checkout/AddCreditCard';
 // AHMED IMPORTS
-import Addresses from "./pages/Checkout/Addresses";
-import AddAddress from "./pages/Checkout/AddAddress";
-import EditAddress from "./pages/Checkout/EditAddress";
-import CreditCards from "./pages/Checkout/CreditCards";
-import AddCreditCard from "./pages/Checkout/AddCreditCard";
-import EditCreditCard from "./pages/Checkout/EditCreditCard";
-import MyForm from "./pages/Checkout/MyForm";
-import Choice from "./pages/Checkout/Choice";
-import Confirmation from "./pages/Checkout/Confirmation";
-import Cart from "./pages/Checkout/Cart";
-
+import Addresses from './pages/Checkout/Addresses';
+import Cart from './pages/Checkout/Cart';
+import Choice from './pages/Checkout/Choice';
+import Confirmation from './pages/Checkout/Confirmation';
+import CreditCards from './pages/Checkout/CreditCards';
+import EditAddress from './pages/Checkout/EditAddress';
+import EditCreditCard from './pages/Checkout/EditCreditCard';
+import MyForm from './pages/Checkout/MyForm';
+import CustomerLanding from './pages/CustomerLanding';
+import EditProduct from './pages/EditProduct';
+import AddEnquiry from './pages/Enquiries/AddEnquiry';
+import AddResponse from './pages/Enquiries/AddResponse';
+import EnquiriesDashboard from './pages/Enquiries/Dashboard';
 // Amelia Imports
-import Enquiries from "./pages/Enquiries/Enquiries";
-import AddEnquiry from "./pages/Enquiries/AddEnquiry";
-import AddResponse from "./pages/Enquiries/AddResponse";
-import UpdateEnquiry from "./pages/Enquiries/UpdateEnquiry";
-import EnquiriesDashboard from "./pages/Enquiries/Dashboard";
-import SelectContent from './pages/Newsletter/SelectContent';
+import Enquiries from './pages/Enquiries/Enquiries';
+import UpdateEnquiry from './pages/Enquiries/UpdateEnquiry';
+import AdminChat from './pages/LiveChat/AdminChat';
+import Chat from './pages/LiveChat/Chat';
 import CreateNewsletter from './pages/Newsletter/CreateNewsletter';
-import Chat from "./pages/LiveChat/Chat";
-import AdminChat from "./pages/LiveChat/AdminChat";
-import ChatWidget from "./components/ChatWidget";
-
+import SelectContent from './pages/Newsletter/SelectContent';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import Reviews from './pages/Reviews';
+import Dashboard from './pages/admin/Dashboard';
+import StaffDashboard from './pages/admin/products/AdminProducts';
+import AdminProducts from './pages/admin/products/AdminProducts';
+import AddReward from './pages/admin/rewards/AddReward';
+import EditReward from './pages/admin/rewards/EditReward';
+import Rewards from './pages/admin/rewards/Rewards';
+import ViewReward from './pages/admin/rewards/ViewReward';
+import EditUser from './pages/admin/user/EditUser';
+import Users from './pages/admin/user/Users';
+import ViewUser from './pages/admin/user/ViewUser';
 // Fran Imports
-import Account from "./pages/customer/user/Account";
-import ResetPassword from "./pages/customer/user/ResetPassword";
-import ForgotPassword from "./pages/customer/user/ForgotPassword";
-import Dashboard from "./pages/admin/Dashboard";
-import ProtectedRoute from "./components/admin/ProtectedRoute";
-import Users from "./pages/admin/user/Users";
-import EditUser from "./pages/admin/user/EditUser";
-import ViewUser from "./pages/admin/user/ViewUser";
-import Rewards from "./pages/admin/rewards/Rewards";
-import Login from "./pages/customer/user/Login";
-import Register from "./pages/customer/user/Register";
-import TermsOfUse from "./pages/customer/user/TermsOfUse";
-import PrivacyPolicy from "./pages/customer/user/PrivacyPolicy";
-import { ToastContainer } from "react-toastify";
-import { Box } from "@mui/material";
-import AddReward from "./pages/admin/rewards/AddReward";
-import EditReward from "./pages/admin/rewards/EditReward";
-import ViewReward from "./pages/admin/rewards/ViewReward";
-import AdminProducts from "./pages/admin/products/AdminProducts";
+import Account from './pages/customer/user/Account';
+import ForgotPassword from './pages/customer/user/ForgotPassword';
+import Login from './pages/customer/user/Login';
+import PrivacyPolicy from './pages/customer/user/PrivacyPolicy';
+import Register from './pages/customer/user/Register';
+import ResetPassword from './pages/customer/user/ResetPassword';
+import TermsOfUse from './pages/customer/user/TermsOfUse';
+
+import './App.css';
+import MyTheme from './themes/MyTheme';
 
 function App() {
   // update in the user context too
@@ -76,7 +77,7 @@ function App() {
 
   // Retrieve user data from localStorage (if available)
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedUser = JSON.parse(localStorage.getItem('user'));
     if (storedUser) {
       setUser(storedUser);
       setLoading(false);
@@ -85,14 +86,14 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem('user', JSON.stringify(user));
       if (isFirstLoad.current) {
         isFirstLoad.current = false; // Skip the first load call
         return; // Don't call the API for the first user set
       }
 
       // Detect if a new login occurred
-      const storedUser = JSON.parse(localStorage.getItem("user"));
+      const storedUser = JSON.parse(localStorage.getItem('user'));
       if (storedUser?.userId !== user.userId) {
         isFirstLoad.current = true; // Treat this as a new "first load"
         return; // Skip API call for the new login
@@ -121,16 +122,16 @@ function App() {
               ...user,
               ...res.data.user,
             };
-            localStorage.setItem("user", JSON.stringify(updatedUser));
+            localStorage.setItem('user', JSON.stringify(updatedUser));
             setLoading(false);
           }
         };
         updateUser();
       } catch (err) {
-        console.error("Failed to update user profile:", err);
+        console.error('Failed to update user profile:', err);
       }
     } else {
-      localStorage.removeItem("user");
+      localStorage.removeItem('user');
       isFirstLoad.current = true; // Reset the first load tracker
     }
   }, [user]);
@@ -140,89 +141,89 @@ function App() {
 
   const sharedRoutes = [
     {
-      url: "/login",
+      url: '/login',
       component: <Login />,
     },
     {
-      url: "/register",
+      url: '/register',
       component: <Register />,
     },
     {
-      url: "/reset-password",
+      url: '/reset-password',
       component: <ResetPassword />,
     },
     {
-      url: "/forgot-password",
+      url: '/forgot-password',
       component: <ForgotPassword />,
     },
     {
-      url: "/terms-of-use",
+      url: '/terms-of-use',
       component: <TermsOfUse />,
     },
     {
-      url: "/account",
+      url: '/account',
       component: <Navigate to="/account/profile" />,
     },
     {
-      url: "/account/:tab",
+      url: '/account/:tab',
       component: <Account />,
     },
     {
-      url: "/privacy-policy",
+      url: '/privacy-policy',
       component: <PrivacyPolicy />,
     },
     {
-      url: "/unauthorized",
+      url: '/unauthorized',
       component: <h1>Unauthorized</h1>,
     },
   ];
 
   const adminRoutes = [
     {
-      url: "/admin/dashboard",
+      url: '/admin/dashboard',
       component: Dashboard,
     },
     {
-      url: "/admin/users",
+      url: '/admin/users',
       component: Users,
     },
     {
-      url: "/admin/users/:id/view",
+      url: '/admin/users/:id/view',
       component: ViewUser,
     },
     {
-      url: "/admin/users/:id/edit",
+      url: '/admin/users/:id/edit',
       component: EditUser,
     },
     {
-      url: "/admin/rewards",
+      url: '/admin/rewards',
       component: Rewards,
     },
     {
-      url: "/admin/rewards/add",
+      url: '/admin/rewards/add',
       component: AddReward,
     },
     {
-      url: "/admin/rewards/:id/edit",
+      url: '/admin/rewards/:id/edit',
       component: EditReward,
     },
     {
-      url: "/admin/rewards/:id/view",
+      url: '/admin/rewards/:id/view',
       component: ViewReward,
     },
     {
-      url: "/admin/products",
+      url: '/admin/products',
       component: AdminProducts,
     },
     {
-      url: "/admin/enquiries",
+      url: '/admin/enquiries',
       component: Enquiries,
     },
   ];
 
   const logout = () => {
     localStorage.clear();
-    window.location = "/";
+    window.location = '/';
   };
 
   return (
@@ -233,9 +234,9 @@ function App() {
             <ToastContainer />
             <Box
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                height: "100vh",
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
               }}
             >
               {/* Navbar is always rendered */}
@@ -245,7 +246,7 @@ function App() {
                 <Route
                   path="/"
                   element={
-                    user?.role == "Admin" ? (
+                    user?.role == 'Admin' ? (
                       <StaffDashboard />
                     ) : (
                       <CustomerLanding />
@@ -282,7 +283,7 @@ function App() {
                     <AddProduct
                       onAddSuccess={() => {
                         // Navigate back to StaffDashboard after adding a product
-                        window.location.href = "/";
+                        window.location.href = '/';
                       }}
                     />
                   }
@@ -322,7 +323,8 @@ function App() {
 
                 <Route path="/adminchat" element={<AdminChat />} />
               </Routes>
-              {!adminRoutes.includes(location.pathname) && <ChatWidget />}
+
+              <ChatWidget />
             </Box>
           </ThemeProvider>
         </Router>
