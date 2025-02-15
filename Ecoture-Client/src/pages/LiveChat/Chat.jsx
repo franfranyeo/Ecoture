598174c0fb83dbc1d87b6a401964158342657a76
jia-xinnnn <233506d@mymail.nyp.edu.sg>
2025-02-15 21:07:22 +0800

    Chat UserId

--- Ecoture-Client/src/pages/LiveChat/Chat.jsx
+++ Ecoture-Client/src/pages/LiveChat/Chat.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import UserContext from "contexts/UserContext";
 import * as signalR from "@microsoft/signalr";
 import "./Chat.css"; // Ensure you have a corresponding CSS file for styling
 
@@ -6,11 +7,13 @@ const Chat = () => {
     const [connection, setConnection] = useState(null);
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
-    const [user, setUser] = useState("User" + Math.floor(Math.random() * 1000));
+    const { user } = useContext(UserContext);
+
+    const userId = user ? `User ${user.userId}` : "Guest " + Math.floor(Math.random() * 1000)
 
     useEffect(() => {
         const newConnection = new signalR.HubConnectionBuilder()
-            .withUrl(`${import.meta.env.VITE_API_BASE_URL}/chatHub?username=${user}`, {
+            .withUrl(`${import.meta.env.VITE_API_BASE_URL}/chatHub?username=${userId}`, {
                 withCredentials: true
             })
             .withAutomaticReconnect()
@@ -20,8 +23,8 @@ const Chat = () => {
             .then(() => console.log("User Connected to SignalR"))
             .catch(err => console.error("Connection error:", err));
 
-        newConnection.on("ReceiveMessage", (user, message) => {
-            setMessages(prevMessages => [...prevMessages, { user, message }]);
+        newConnection.on("ReceiveMessage", (userId, message) => {
+            setMessages(prevMessages => [...prevMessages, { userId, message }]);
         });
 
         setConnection(newConnection);
@@ -35,7 +38,7 @@ const Chat = () => {
 
     const sendMessage = async () => {
         if (connection && message.trim() !== "") {
-            await connection.send("SendMessage", user, message);
+            await connection.send("SendMessage", userId, message);
             setMessage(""); // Clear input after sending
         }
     };
@@ -44,8 +47,8 @@ const Chat = () => {
         <div className="chat-container">
             <div className="chat-box">
                 {messages.map((msg, index) => (
-                    <div key={index} className={`message ${msg.user === user ? "sent" : "received"}`}>
-                        <strong>{msg.user}:</strong> {msg.message}
+                    <div key={index} className={`message ${msg.userId === userId ? "sent" : "received"}`}>
+                        <strong>{msg.userId}:</strong> {msg.message}
                     </div>
                 ))}
             </div>
